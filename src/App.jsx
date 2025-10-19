025fc255c307d4eec0e6ed4734ab8f459af3d92d
theadesolasarah <williamssarahadesola@gmail.com>
2025-10-19 15:26:09 +0100

    Update App.jsx with /favorites route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SearchBox from './components/SearchBox';
-import BookResults from './components/BookResults';
-import BookDetails from './components/BookDetails';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SearchBox from "./components/SearchBox";
+import BookResults from "./components/BookResults";
+import BookDetails from "./components/BookDetails";
+import Favorites from "./components/Favorites";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<SearchBox />} />
         <Route path="/results" element={<BookResults />} />
         <Route path="/book/:id" element={<BookDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </Router>
   );
